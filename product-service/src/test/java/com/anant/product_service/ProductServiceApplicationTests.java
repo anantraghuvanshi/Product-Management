c2b2fb4c97a537ba90db7c37ce95211d13458504
Anant <anantsinghraghuvanshi1@gmail.com>
2024-06-02 22:34:26 +0530

    Initial commit

---
+++ product-service/src/test/java/com/anant/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anant.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
