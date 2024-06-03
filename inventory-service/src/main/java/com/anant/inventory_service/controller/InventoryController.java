b602ae0539fdab47946bc6cfa375206e386f6cbc
Anant <anantsinghraghuvanshi1@gmail.com>
2024-06-03 16:55:42 +0530

    inventory service done

---
+++ inventory-service/src/main/java/com/anant/inventory_service/controller/InventoryController.java
@@ -0,0 +1,19 @@
+package com.anant.inventory_service.controller;
+
+import com.anant.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+        return inventoryService.isInStock(skuCode);
+    }
+}
