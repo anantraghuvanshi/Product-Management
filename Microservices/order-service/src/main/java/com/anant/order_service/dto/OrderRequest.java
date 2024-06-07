47091259d5dec823c83c3ea85fe0ed9290acafb5
Anant <anantsinghraghuvanshi1@gmail.com>
2024-06-07 21:16:31 +0530

    all service merge

---
+++ Microservices/order-service/src/main/java/com/anant/order_service/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.anant.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+
+    private List<OrderListItemsDto> orderListItemsDtoList;
+}
---
+++ Microservices/order-service/src/main/java/com/anant/order_service/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.anant.order_service.repository;
+
+import com.anant.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
