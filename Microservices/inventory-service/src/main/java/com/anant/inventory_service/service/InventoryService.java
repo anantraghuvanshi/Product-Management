a1b1200ed402ca7f4a116fd383c9eccd8963383e
Anant <anantsinghraghuvanshi1@gmail.com>
2024-06-12 23:56:20 +0530

    inventory service update for inter service communication

--- Microservices/inventory-service/src/main/java/com/anant/inventory_service/service/InventoryService.java
+++ Microservices/inventory-service/src/main/java/com/anant/inventory_service/service/InventoryService.java
@@ -1,9 +1,14 @@
 package com.anant.inventory_service.service;
 
+import com.anant.inventory_service.dto.InventoryResponse;
 import com.anant.inventory_service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +16,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+                .stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }
